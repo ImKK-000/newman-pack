bcb109fb0094a64906b4f6d8a8593b581edfd5cc
Nattakit Boonyang <nattakit.boonyang@gmail.com>
2018-11-10 00:29:44 +0700

    [Edited]: use npm from node_modules

--- run_test.sh
+++ run_test.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 TOOLS_PATH=$PWD/tools
-export PATH=$TOOLS_PATH/node-v10.13.0-darwin-x64/bin:$TOOLS_PATH/node_modules/.bin:$PATH
+export PATH=$TOOLS_PATH/node_modules/.bin:$TOOLS_PATH/node-v10.13.0-darwin-x64/bin:$PATH
 echo node= `node -v`
 echo npm= `npm -v`
 echo newman= `newman -v`
