25df2550c531ba39e31d093b951c3f43d33b1f3b
Nattakit Boonyang <nattakit.boonyang@gmail.com>
2018-11-10 14:33:29 +0700

    [Added]: run atdd to run test script

--- run_test.sh
+++ run_test.sh
@@ -1,6 +1,12 @@
 #!/bin/sh
+echo \#\# set tools to test environment
 TOOLS_PATH=$PWD/tools
 export PATH=$TOOLS_PATH/node_modules/.bin:$TOOLS_PATH/node-v10.13.0-darwin-x64/bin:$PATH
+
+echo \#\# version
 echo node= `node -v`
 echo npm= `npm -v`
 echo newman= `newman -v`
+
+echo \#\# run acceptance test
+newman run atdd/api/example_test_collection.json
