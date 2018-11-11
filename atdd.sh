c88ba45aed9b53c9fdb7e2d1f08c5c00ce47c892
Nattakit Boonyang <nattakit.boonyang@gmail.com>
2018-11-11 08:17:23 +0700

    [Edited]: move hard newman script from run_test.sh to atdd.sh

---
+++ atdd.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+newman run atdd/api/example_test_collection.json
--- run_test.sh
+++ run_test.sh
@@ -3,10 +3,10 @@ echo \#\# set tools to test environment
 TOOLS_PATH=$PWD/tools
 export PATH=$TOOLS_PATH/node_modules/.bin:$TOOLS_PATH/node-v10.13.0-darwin-x64/bin:$PATH
 
-echo \#\# version
+echo \#\# tools version
 echo node= `node -v`
 echo npm= `npm -v`
 echo newman= `newman -v`
 
 echo \#\# run acceptance test
-newman run atdd/api/example_test_collection.json
+source atdd.sh
